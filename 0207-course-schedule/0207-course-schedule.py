008d1a9a8619f4176c686d78a22ddffdce0b34d4
Houdayfa Housny <90914113+Houdayfahousny@users.noreply.github.com>
2025-09-02 19:13:06 +0100

    Time: 8 ms (23.77%), Space: 20 MB (8.24%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preq={c: [] for c in range(numCourses)}
+        for crs,pre in prerequisites:
+            preq[crs].append(pre)
+
+       
+        visit,cycle=set(),set()
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+
+            cycle.add(crs)
+            for pre in preq[crs]:
+                if dfs(pre)==False:
+                    return False
+
+            cycle.remove(crs)
+            visit.add(crs)
+            
+            return True
+
+        for c in range(numCourses):
+            if dfs(c)==False:
+                return False
+
+        return True
+
+
