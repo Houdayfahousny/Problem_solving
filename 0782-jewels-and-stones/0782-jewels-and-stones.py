75a2d6208ded41f99556ffbd779f16263a8f58eb
Houdayfa Housny <90914113+Houdayfahousny@users.noreply.github.com>
2025-09-11 03:41:29 +0100

    Time: 0 ms (100%), Space: 17.8 MB (52.11%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        output=0
+        for i in stones:
+            if i in jewels:
+                output+=1
+
+        return output
